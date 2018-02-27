314bc04df235958e1f437a2c37a2fb66ae454a57
Luis <lalvarezguillen@gmail.com>
2018-02-27 22:12:02 +0100

    working w/o kue

--- get_saime_data.js
+++ get_saime_data.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
 const MongoClient = require('mongodb').MongoClient;
+const cheerio = require('cheerio');
+const delay = require('delay');
+const range = require('range');
+const kue = require('kue');
 
 function createCIGen(initial, nat) {
     if (nat.toUpperCase() != 'V' && nat.toUpperCase() != 'E') {
@@ -16,10 +20,10 @@ function createCIGen(initial, nat) {
     }
 }
 
-async function getSaimeData(endpoint, ci, nat){
+async function getSaimeData(ci, nat){
     try {
         const resp = await axios.post(
-            endpoint,
+            config.saimeUrl,
             {
                 cedula: ci,
                 nacionalidad: nat
@@ -34,19 +38,42 @@ async function getSaimeData(endpoint, ci, nat){
 }
 
 async function getCneData(ci, nat) {
-    const url = `${config.cneUrlBase}?nacionalidad=${nat}&cedula=${ci}`
+    const url = `${config.cneUrlBase}?nacionalidad=${nat}&cedula=${ci}`;
+    const key = `${nat}${ci}`;
     try {
         const resp = await axios.get(url);
-        console.log(resp.status);
-        console.log(resp.data);
-        return resp.data;
+        const dom = cheerio.load(resp.data);
+        if (isRegisteredInCne(dom)) {
+            return parseCneData(dom);
+        }
     }
     catch (error){
         console.log(error);
     }
 }
 
-async function storeData(ci, data) {
+function isRegisteredInCne(dom){
+    return (
+        !Boolean(dom('td').eq(11).text().match('no corresponde')) &&
+        !Boolean(dom('td').eq(15).text().match('no se encuentra'))
+    );
+}
+
+function parseCneData(dom) {
+    const data = {
+        cedula: dom('td').eq(11).text(),
+        nombre: dom('td').eq(13).text(),
+        estado: dom('td').eq(15).text(),
+        municipio: dom('td').eq(17).text(),
+        parroquia: dom('td').eq(19).text(),
+        centro: dom('td').eq(21).text(),
+        direccion: dom('td').eq(23).text()
+    }
+    console.log(data);
+    return data;
+}
+
+async function storeData(data) {
     let client, db, coll;
     try {
         client = await MongoClient.connect(config.mongoUrl);
@@ -56,7 +83,7 @@ async function storeData(ci, data) {
         return
     }
     db = client.db(config.mongoDB);
-    coll = db.collection(config.mongoCollection);
+    coll = db.collection(config.cneCollection);
 
     try {
         await coll.insertOne(data);
@@ -67,16 +94,34 @@ async function storeData(ci, data) {
     console.log('listo el pollo')
 }
 
-
 const config = {
     saimeUrl: 'https://tramites.saime.gob.ve/index.php?r=usuario/usuario/BuscarSaimeContacto',
     cneUrlBase: 'http://www.cne.gov.ve/web/registro_electoral/ce.php',
     mongoUrl: 'mongodb://localhost:27017',
-    mongoCollection: 'saime',
+    saimeCollection: 'saime',
+    cneCollection: 'cne',
     mongoDB: 'venez',
-
 }
 
-getSaimeData(config.saimeUrl, 3000000, 'V')
+async function getCneUser(ci, nat){
+    const data = await getCneData(ci, nat);
+    if (data) {
+        const key = `${nat}${ci}`
+        data['_id'] = key;
+        storeData(data);
+    }
+}
+
+function crawlCne(){
+    const nat = 'V';
+    const cis = range.range(19500001, 19500100);
+    for (let ci of cis) {
+        console.log(ci, nat);
+        getCneUser(ci, nat);
+        delay(500);
+    }
+}
+// getSaimeData(3000000, 'V')
 // getCneData(3000000, 'V')
 // storeData(9000, {data:'some data'})
+crawlCne()
